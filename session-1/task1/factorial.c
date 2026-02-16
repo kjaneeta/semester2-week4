e9325169d414a830b6d0a0cf5a9cc5cd503fe8e1
kjaneeta <jzps0355@leeds.ac.uk>
2026-02-16 11:40:44 +0000

    c practice array

--- session-1/task1/factorial.c
+++ session-1/task1/factorial.c
@@ -8,6 +8,18 @@
  int main( void ) {
     int f[20];
 
+    f[0] = 1;
+
+   for(int i = 0; i<20; ++i)
+   {
+      f[i] = i * f[i-1];
+   }
+
+   for(int i = 0; i<20; ++i)
+   {
+      printf('Factorial of ' + i + ' = ' + f[i]);
+   }
+
     /*
     Code to compute the factorial of each array index
     Print your final answer
